f065b55f6473584c2ed5ea63929a381df6a7c10e
Muhammad Usman <usman.mzahoor@emumba.com>
2025-05-22 15:58:03 +0500

    feat (Home page): Add PageHeadingContainer

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,12 +1,12 @@
-import { MainLayout } from '@/components';
+import { MainLayout, PageHeadingContainer } from '@/components';
+import { AppStrings } from '@/constants';
 import type { JSX } from 'react';
 
 export function HomePage(): JSX.Element {
   return (
     <MainLayout>
-      <>
-        <h1>Home page</h1>
-      </>
+      <PageHeadingContainer title={AppStrings.PublicGists} />
+      <h1>Put table here</h1>
     </MainLayout>
   );
 }
