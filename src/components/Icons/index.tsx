e389e277caa6f6612261e8314cee77df214f2c0f
Muhammad Usman <usman.mzahoor@emumba.com>
2025-05-26 16:22:34 +0500

    feat: Add change listing button

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,5 +1,9 @@
 import SearchIcon from '@mui/icons-material/Search';
+import TableIcon from '@mui/icons-material/FormatListBulletedSharp';
+import GridIcon from '@mui/icons-material/Subject';
 
 export const Icons = {
   Search: SearchIcon,
+  Table: TableIcon,
+  Grid: GridIcon,
 } as const;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,46 @@
-import { MainLayout, PageHeadingContainer } from '@/components';
+import { Icons, MainLayout, PageHeadingContainer } from '@/components';
 import { AppStrings } from '@/constants';
-import { type JSX } from 'react';
+import {
+  ToggleButton,
+  ToggleButtonGroup,
+  type ToggleButtonGroupProps,
+} from '@mui/material';
+import { useState, type JSX } from 'react';
+import { GistsLayouts, type GistsLayout } from './types';
 
 export function HomePage(): JSX.Element {
+  const [selectedLayout, setSelectedLayout] = useState<GistsLayout>(
+    GistsLayouts.Table
+  );
+
+  const handleValueChange: ToggleButtonGroupProps['onChange'] = (
+    _,
+    newValue
+  ) => {
+    setSelectedLayout(newValue);
+  };
+
+  const renderListLayoutToggle = () => (
+    <ToggleButtonGroup
+      exclusive
+      value={selectedLayout}
+      onChange={handleValueChange}
+    >
+      <ToggleButton value={GistsLayouts.Grid}>
+        <Icons.Grid />
+      </ToggleButton>
+      <ToggleButton value={GistsLayouts.Table}>
+        <Icons.Table />
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+
   return (
     <MainLayout>
-      <PageHeadingContainer title={AppStrings.PublicGists} />
+      <PageHeadingContainer
+        title={AppStrings.PublicGists}
+        RightComponent={renderListLayoutToggle()}
+      />
       <h1>Put table here</h1>
     </MainLayout>
   );
