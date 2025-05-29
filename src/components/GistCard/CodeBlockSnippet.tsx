54b181488d5101bf196498b584b6e6cbc384000b
Muhammad Usman <usman.mzahoor@emumba.com>
2025-05-29 09:24:55 +0500

    feat (GistInfo): Add basic code block design

--- src/components/GistCard/CodeBlockSnippet.tsx
+++ src/components/GistCard/CodeBlockSnippet.tsx
@@ -1,27 +1,31 @@
 import { AppStrings } from '@/constants';
 import { useBooleanState } from '@/hooks';
 import { theme } from '@/styles';
-import { Stack, styled } from '@mui/material';
+import { Stack, styled, Typography } from '@mui/material';
 import { useMemo, type JSX } from 'react';
 import type { CodeBlockSnippetProps } from './types';
 import { getFilename } from '@/utils';
 
 const ContainerStack = styled(Stack)(({ theme }) => ({
-  boxSizing: 'border-box',
+  position: 'relative',
+  color: theme.palette.secondary.main,
   '&:hover': {
     border: `1px solid ${theme.palette.primary.main}`,
+    borderTopLeftRadius: theme.shape.borderRadius,
+    borderTopRightRadius: theme.shape.borderRadius,
   },
 }));
 
 export function CodeBlockSnippet({
   files,
 }: CodeBlockSnippetProps): JSX.Element {
-  const [, hover, unHover] = useBooleanState();
+  const [hovered, hover, unHover] = useBooleanState();
   const filename = useMemo(() => getFilename(files), [files]);
 
   return (
     <ContainerStack onMouseEnter={hover} onMouseLeave={unHover}>
-      <Stack sx={{ height: 200 }}>
+      <Stack sx={{ height: 200 }}></Stack>
+      {hovered && (
         <Stack
           sx={{
             position: 'absolute',
@@ -29,13 +33,19 @@ export function CodeBlockSnippet({
             top: 0,
             zIndex: 10,
             background: theme.palette.primary.main,
+            p: 1,
+            maxWidth: '80%',
+            maxHeight: '40%',
           }}
         >
-          {AppStrings.View}
-
-          {filename}
+          <Typography fontSize={11}>
+            {AppStrings.View}{' '}
+            <Typography fontSize={11} fontWeight={700} component="span">
+              {filename}
+            </Typography>
+          </Typography>
         </Stack>
-      </Stack>
+      )}
     </ContainerStack>
   );
 }
