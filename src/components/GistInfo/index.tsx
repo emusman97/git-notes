10cae16bb2d874fdd0553ccc3a754bf2a857103e
Muhammad Usman <usman.mzahoor@emumba.com>
2025-05-29 09:24:13 +0500

    fix (GistInfo): Show filename on for gist name

--- src/components/GistInfo/index.tsx
+++ src/components/GistInfo/index.tsx
@@ -1,19 +1,21 @@
-import type { JSX } from 'react';
+import { useMemo, type JSX } from 'react';
 import type { GistInfoProps } from './types';
 import Typography from '@mui/material/Typography';
 import { AppStrings } from '@/constants';
 import Stack from '@mui/material/Stack';
-import { timeAgo } from '@/utils';
+import { getFilename, timeAgo } from '@/utils';
 
 export function GistInfo({ data, ...restProps }: GistInfoProps): JSX.Element {
   const user = data.user ?? data.owner ?? {};
+
+  const filename = useMemo(() => getFilename(data.files ?? {}), [data.files]);
 
   return (
     <Stack gap={0.5} {...restProps}>
       <Typography>
         {user.login ?? AppStrings.JohnDoe} /{' '}
         <Typography component="span" fontWeight={600}>
-          {AppStrings.GistName}
+          {filename}
         </Typography>
       </Typography>
 
