c3b22952ace61d8cbb1c6fb05c92e35f0a3b40b0
Muhammad Usman <usman.mzahoor@emumba.com>
2025-06-03 10:23:49 +0500

    fix (theme): Fix dark mode colors

--- src/pages/HomePage/components/Table/StyledTableCell.tsx
+++ src/pages/HomePage/components/Table/StyledTableCell.tsx
@@ -2,9 +2,9 @@ import { styled, TableCell, tableCellClasses } from '@mui/material';
 
 export const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.custom.tableHeader?.main,
+    backgroundColor: theme.palette.tableHeader?.main,
   },
   [`&.${tableCellClasses.footer}`]: {
-    backgroundColor: theme.custom.tableHeader?.main,
+    backgroundColor: theme.palette.tableHeader?.main,
   },
 }));
